1aee9462ad7bf2f86af77895b3b7ee79acac0cb3
conceptdecorators <conceptdecorators@email.com>
2021-06-08 13:05:31 -0400

    updated console log process.env

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -1,5 +1,6 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// console.log(process.env.EMAIL_TO);
 
 exports.emailFeedback = (req, res) => {
   // console.log(req.body);
