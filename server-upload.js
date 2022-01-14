c18e06ee6dbadced04f07a18c565394e2e0c0f30
conceptdecorators <conceptdecorators@email.com>
2022-01-14 15:35:21 -0500

    file changes

--- server-upload.js
+++ server-upload.js
@@ -1,26 +1,32 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv").config();
-
-// console.log(process.env);
+const path = require("path");
 
 // import routes
 const feedbackRoutes = require("./routes/feedback");
 
 // app
 const app = express();
-
+//  can you go to browser?
 // middlewares
+// app.use(express.static(path.resolve(__dirname + "/../client/build")));
 app.use(morgan("dev"));
-app.use(bodyParser.json());
+app.use(express.json());
 // app.use(express.json());
 app.use(cors());
 
 // routes
 app.use("/api", feedbackRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Server is running")
+})
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname + "/../client/build/index.html"));
+// });
 // port
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
